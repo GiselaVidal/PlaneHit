d2820f5d4236e91f3813d301e291796dead82236
66aran66 <apuentel@edu.tecnocampus.cat>
2025-05-21 15:49:14 +0200

    spawner

---
+++ NavecitaC/Source/Game/Explosion.cs
@@ -0,0 +1,21 @@
+﻿using SFML.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcGame
+{
+    internal class Explosion
+    {
+        public Explosion() 
+        {
+
+        }
+        public override void Update(float dt)
+        {
+
+        }
+    }
+}
