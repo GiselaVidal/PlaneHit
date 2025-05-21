ec5b95409d1ec724f1768541f7f82cece712764a
Nil Puente <npuentel@edu.tecnocampus.cat>
2025-05-21 12:01:55 +0200

    nse

--- NavecitaC/Source/Game/Spaceship.cs
+++ NavecitaC/Source/Game/Spaceship.cs
@@ -15,7 +15,7 @@ namespace TcGame
         {
 
             Layer = ELayer.Front;
-            Sprite = new Sprite(new Texture("Data/Textures/Player/spaceship.png"));
+            Sprite = new Sprite(new Texture("Data/Textures/Player/falcon.png"));
             Position = new SFML.System.Vector2f(Engine.Get.Window.Size.X / 2, Engine.Get.Window.Size.Y / 2 + 100); 
             Center();
             
